c89f1d0ff7e177b5d590db4675ff9b00378614e3
studies-admyu <studies-admyu@users.noreply.github.com>
2018-03-10 12:34:31 +0300

    Added pb2 data reader.

---
+++ data_readers/pb2_reader/pb2_document_reader.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import gzip
+
+from data_readers.pb2_reader.gs_pb2 import document_length
+from data_readers.pb2_reader.big_pb2 import ImageStruct
+
+class Pb2DocumentReader:
+    _DOCUMENT_LENGTH_SIZE = 5
+    def __init__(self, path):
+        self._stream = None
+        
+        if not os.path.isfile(path):
+            raise Exception('File %s is not found' % (path))
+        if path.endswith('.gz'):
+            self._stream = gzip.open(path, 'rb')
+        else:
+            self._stream = open(path, 'rb')
+    
+    def __iter__(self):
+        while True:
+            document_len_struct = document_length()
+            
+            document_length_bytes = self._stream.read(
+                self._DOCUMENT_LENGTH_SIZE
+            )
+            if len(document_length_bytes) < self._DOCUMENT_LENGTH_SIZE:
+                return
+            
+            document_len_struct = document_length()
+            document_len_struct.ParseFromString(
+                document_length_bytes
+            )
+            image_struct_size = document_len_struct.length
+            
+            img_struct_bytes = self._stream.read(image_struct_size)
+            img_struct = ImageStruct()
+            img_struct.ParseFromString(
+                img_struct_bytes
+            )
+            
+            yield img_struct
---
+++ pb2_extract.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from data_readers.pb2_reader.pb2_document_reader import Pb2DocumentReader
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        sys.stderr.write(
+            'Need at least two arguments: <extract_dir> <gs_file1> ' +
+            '[<gs_file2> ...]\n'
+        )
+        sys.exit(1)
+    
+    dir_to_extract = sys.argv[1]
+    for filename in sys.argv[2:]:
+        try:
+            for image in Pb2DocumentReader(filename):
+                with open(
+                    os.path.join(
+                        dir_to_extract, 
+                        str(image.imageId.imageHash) + '.jpeg'),
+                    'wb'
+                ) as f:
+                    f.write(image.content)
+            
+        except Exception as e:
+            sys.stderr.write('ERROR: %s', str(e))
+            continue
